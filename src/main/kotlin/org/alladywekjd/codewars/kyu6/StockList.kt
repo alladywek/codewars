71947435da42b5f1c9135db289f2c5241cd12301
Abbas Abbasov <>
2019-11-23 18:19:36 +0200

    add 2 tasks from code wars

---
+++ src/main/kotlin/org/alladywekjd/codewars/kyu6/StockList.kt
@@ -0,0 +1,9 @@
+package org.alladywekjd.codewars.kyu6
+
+object StockList {
+
+    fun stockSummary(lstOfArt: Array<String>, lstOfCat: Array<String>): String {
+        val repository = lstOfArt.groupBy({ stock -> stock[0].toString() }, { stock -> stock.split(" ")[1].toInt() })
+        return lstOfCat.joinToString(" - ") { category -> "($category : ${repository[category]?.sum() ?: 0})" }
+    }
+}
