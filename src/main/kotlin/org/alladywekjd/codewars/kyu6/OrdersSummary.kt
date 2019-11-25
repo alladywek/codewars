01b8e877f3ee76a637a01bf5a588955dad618338
Abbas Abbasov <>
2019-11-25 11:47:22 +0200

    update task from code wars

--- src/main/kotlin/org/alladywekjd/codewars/kyu6/OrdersSummary.kt
+++ src/main/kotlin/org/alladywekjd/codewars/kyu6/OrdersSummary.kt
@@ -1,77 +1,33 @@
 package org.alladywekjd.codewars.kyu6
 
 import java.math.BigDecimal
-import java.util.regex.Matcher
 
 object OrdersSummary {
 
-    sealed class Order {
-        data class CorrectOrder(
-                val quote: String,
-                val quantity: Int,
-                val price: BigDecimal,
-                val totalPrice: BigDecimal,
-                val status: String) : Order()
-
-        data class IncorrectOrder(val src: String) : Order()
-    }
-
     fun balanceStatements(lst: String): String {
-        val (correctOrders, incorrectOrders) = lst
-                .split(",\\s*".toRegex())
-                .map(this::parseOrder)
-                .partition {
-                    when (it) {
-                        is Order.CorrectOrder -> true
-                        is Order.IncorrectOrder -> false
-                    }
-                }
-        val buyPrice = getTotalPriceByStatus(correctOrders, "B")
-        val sellPrice = getTotalPriceByStatus(correctOrders, "S")
-
-        return "Buy: $buyPrice Sell: $sellPrice${buildIncorrectOrdersAsString(incorrectOrders)}"
-    }
-
-    private fun parseOrder(orderString: String): Order {
         val orderPattern = "(?<quote>(\\w|\\.)+)\\s(?<quantity>\\d+)\\s(?<price>\\d+\\.\\d+)\\s(?<status>[BS])".toPattern()
-        val matcher = orderPattern.matcher(orderString)
-        return if (matcher.matches()) {
-            buildCorrectOrder(matcher)
-        } else
-            Order.IncorrectOrder(orderString)
-    }
 
-    private fun buildCorrectOrder(matcher: Matcher): Order.CorrectOrder {
-        val quantity = matcher.group("quantity").toInt()
-        val price = BigDecimal(matcher.group("price"))
-        return Order.CorrectOrder(
-                quote = matcher.group("quote"),
-                quantity = quantity,
-                price = price,
-                totalPrice = price.multiply(BigDecimal(quantity)),
-                status = matcher.group("status")
+        var buyPrice = BigDecimal.ZERO
+        var sellPrice = BigDecimal.ZERO
+        val badOrders = mutableListOf<String>()
+        for (orderString in lst.split(",\\s".toRegex())) {
+            val matcher = orderPattern.matcher(orderString)
+            if (matcher.matches()) {
+                if (matcher.group("status") == "B")
+                    buyPrice = buyPrice.plus(BigDecimal(matcher.group("price")).multiply(BigDecimal(matcher.group("quantity").toInt())))
+                if (matcher.group("status") == "S")
+                    sellPrice = sellPrice.plus(BigDecimal(matcher.group("price")).multiply(BigDecimal(matcher.group("quantity").toInt())))
+            } else {
+                if (orderString.trim().isNotEmpty()) {
+                    badOrders.add("$orderString ;")
+                }
+            }
+        }
+        val badOrdersString = if (badOrders.isEmpty()) "" else badOrders.joinToString(
+                prefix = "; Badly formed ${badOrders.size}: ",
+                separator = ""
         )
-    }
 
-    private fun getTotalPriceByStatus(orders: List<Order>, status: String): String {
-        val filteredOrders = orders
-                .map { it as Order.CorrectOrder }
-                .filter { it.status == status }
-        return if (filteredOrders.isEmpty()) "0" else filteredOrders
-                .map { it.totalPrice }
-                .reduce { sum, orderTotalPrice -> sum.plus(orderTotalPrice) }
-                .toBigInteger()
-                .toString()
-    }
-
-    private fun buildIncorrectOrdersAsString(orders: List<Order>): String {
-        return if (orders.isEmpty()) "" else
-            orders
-                    .map { it as Order.IncorrectOrder }
-                    .joinToString(
-                            prefix = "; Badly formed ${orders.size}: ",
-                            separator = " ;",
-                            postfix = " ;"
-                    ) { it.src }
+        return "Buy: ${buyPrice.toBigInteger()} Sell: ${sellPrice.toBigInteger()}${badOrdersString}"
     }
 }
